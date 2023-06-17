e444a0caef055421aa77a22f63d13b2c923fc995
Videk3 <vid.berlocnik2@scv.si>
2023-06-17 16:34:14 +0200

    Added create function in Lunch service

--- src/lunch/lunch.service.ts
+++ src/lunch/lunch.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Lunch } from '../entities/lunch.entity';
 import { DeleteResult, Repository } from 'typeorm';
+import { CreateLunchDto } from './dto/create-lunch.dto';
 
 @Injectable()
 export class LunchService {
@@ -15,6 +16,10 @@ export class LunchService {
 
   async findOne(id: number): Promise<Lunch> {
     return await this.lunchRepository.findOneBy({ id });
+  }
+  async create(createLunchDto: CreateLunchDto): Promise<Lunch> {
+    const lunch: Lunch = this.lunchRepository.create(createLunchDto);
+    return await this.lunchRepository.save(lunch);
   }
 
   remove(id: number): Promise<DeleteResult> {

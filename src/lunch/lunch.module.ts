7308deae905f13837ab7468d0b8080cd2a214406
Videk3 <vid.berlocnik2@scv.si>
2023-06-17 17:01:35 +0200

    Added TypeOrmModule import

--- src/lunch/lunch.module.ts
+++ src/lunch/lunch.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LunchController } from './lunch.controller';
 import { LunchService } from './lunch.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lunch } from '../entities/lunch.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Lunch])],
   controllers: [LunchController],
   providers: [LunchService],
 })
